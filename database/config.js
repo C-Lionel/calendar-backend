329cd8c1d1380957abb5efb277c1042e9bfcd012
lionel cancellieri <lio2014vt@gmail.com>
2023-04-04 23:27:24 -0300

    updated

--- database/config.js
+++ database/config.js
@@ -1,15 +1,27 @@
-
 const mongoose = require('mongoose');
 
-const dbConnection = async () => {
+
+const dbConnection = async() => {
+
     try {
-        await mongoose.connect(process.env.DB_CNN);
-        console.log('DB Online')
+        
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('DB Online');
+
+
     } catch (error) {
         console.log(error);
-        throw new Error('Error a la hora de inicializar la base de datos..')
+        throw new Error('Error a la hora de inicializar BD');
     }
+
+
 }
+
 
 module.exports = {
     dbConnection
